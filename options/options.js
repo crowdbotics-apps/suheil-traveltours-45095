4790ab73ff154e9584f943b0dcbe421877284b21
Crowdbotics <team@crowdbotics.com>
2023-12-19 09:34:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "suheil-traveltours-45095",
+  url: "https://suheil-traveltours-45095.botics.co",
+  api: "https://suheil-traveltours-45095.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
